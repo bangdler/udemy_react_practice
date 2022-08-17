91dfca976e61a904dc392c2a348d5092074143dd
bangdler <zbthz90@gmail.com>
2022-08-18 00:03:00 +0900

    chore: create-react-app 설치

---
+++ simplediary/README.md
@@ -0,0 +1,5 @@
+## simple diary 만들기
+
+## 컴포넌트
+
+<img alt="img.png" height="400" src="img.png" width="450"/>
---
+++ simplediary/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        {"하이"}
+    </div>
+  );
+}
+
+export default App;
