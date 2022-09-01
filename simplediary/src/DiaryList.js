9c4c59253b77bbaa79b476471d518ec7036463ff
bangdler <zbthz90@gmail.com>
2022-09-02 00:39:08 +0900

    feat: useMemo 활용, 최근 작성 일기 개수 표기

--- simplediary/src/DiaryList.js
+++ simplediary/src/DiaryList.js
@@ -1,11 +1,24 @@
 import DiaryItem from './DiaryItem';
+import { useMemo } from 'react';
 
 const DiaryList = ({ data, onRemove, onEdit }) => {
+  const getRecent2daysDiary = useMemo(() => {
+    console.log('getRecent!');
+    const today = new Date().setHours(0, 0, 0, 0);
+    const numOfRecentDiary = data.filter(it => {
+      const curDate = new Date(it.createdDate).setHours(0, 0, 0, 0);
+      const diff = today - curDate;
+      return diff < 2 * 24 * 60 * 60 * 1000; // 2일 차이까지
+    }).length;
+    return numOfRecentDiary;
+  }, [data.length]);
+
   return (
     <div className="DiaryList">
       <h2>일기리스트</h2>
       <div className="DiaryListContainer">
         <h4>{data.length}개의 일기가 있습니다.</h4>
+        <h4>최근 2일 간 작성한 일기 개수 : {getRecent2daysDiary}</h4>
         {data.map(diary => (
           <DiaryItem key={diary.id} onRemove={onRemove} onEdit={onEdit} {...diary} />
         ))}
